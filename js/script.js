c89fb0bbc369326c1203c8dc24af674ebcfa16b9
oliver-gorst <oliver.gorst@outlook.com>
2021-05-29 00:20:39 -0400

    all elements able to be hidden on click, absolutely brutal way of doing it on Javascript using 48 functions, there has to be a better way

--- js/script.js
+++ js/script.js
@@ -1,5 +1,415 @@
-function courseDiv() {
-    var v = document.getElementById("course");
+function hideCourseImage1() {
+    var v = document.getElementById("MarioKartStadium");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage2() {
+    var v = document.getElementById("WaterPark");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+ function hideCourseImage3() {
+    var v = document.getElementById("SweetSweetCanyon");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage4() {
+    var v = document.getElementById("ThwompRuins");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage5() {
+    var v = document.getElementById("MooMooMeadows");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage6() {
+    var v = document.getElementById("MarioCircuitGBA");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+ function hideCourseImage7() {
+    var v = document.getElementById("CheepCheepBeach");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage8() {
+    var v = document.getElementById("ToadsTurnpike");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+ function hideCourseImage9() {
+    var v = document.getElementById("MarioCircuitFlower");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage10() {
+    var v = document.getElementById("ToadHarbor");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+ function hideCourseImage11() {
+    var v = document.getElementById("TwistedMansion");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage12() {
+    var v = document.getElementById("ShyGuyFalls");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage13() {
+    var v = document.getElementById("DryDryDesert");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage14() {
+    var v = document.getElementById("DonutPlains3");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+ function hideCourseImage15() {
+    var v = document.getElementById("RoyalRaceway");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage16() {
+    var v = document.getElementById("DKJungle");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage17() {
+    var v = document.getElementById("SunshineAirport");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage18() {
+    var v = document.getElementById("DolphinShoals");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+ function hideCourseImage19() {
+    var v = document.getElementById("Electrodome");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage20() {
+    var v = document.getElementById("MountWario");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage21() {
+    var v = document.getElementById("WarioStadium");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage22() {
+    var v = document.getElementById("SherbetLand");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+ function hideCourseImage23() {
+    var v = document.getElementById("MusicPark");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage24() {
+    var v = document.getElementById("YoshiValley");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage25() {
+    var v = document.getElementById("CloudtopCruise");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage26() {
+    var v = document.getElementById("BoneDryDunes");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+ function hideCourseImage27() {
+    var v = document.getElementById("BowsersCastle");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage28() {
+    var v = document.getElementById("RainbowRoadSpecial");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage29() {
+    var v = document.getElementById("TikTokClock");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage30() {
+    var v = document.getElementById("PiranhaPlantSlide");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+ function hideCourseImage31() {
+    var v = document.getElementById("GrumbleVolcano");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage32() {
+    var v = document.getElementById("RainbowRoadLightning");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage33() {
+    var v = document.getElementById("YoshiCircuit");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage34() {
+    var v = document.getElementById("ExcitebikeArena");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+ function hideCourseImage35() {
+    var v = document.getElementById("DragonDriftway");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage36() {
+    var v = document.getElementById("MuteCity");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage37() {
+    var v = document.getElementById("WariosGoldMine");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage38() {
+    var v = document.getElementById("RainbowRoadIlluminati");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+ function hideCourseImage39() {
+    var v = document.getElementById("IceIceOutpost");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage40() {
+    var v = document.getElementById("HyruleCircuit");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage41() {
+    var v = document.getElementById("BabyPark");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage42() {
+    var v = document.getElementById("CheeseLand");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+ function hideCourseImage43() {
+    var v = document.getElementById("WildWoods");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage44() {
+    var v = document.getElementById("AnimalCrossing");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage45() {
+    var v = document.getElementById("NeoBowserCity");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage46() {
+    var v = document.getElementById("RibbonRoad");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+ function hideCourseImage47() {
+    var v = document.getElementById("SuperBellSubway");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
+
+ function hideCourseImage48() {
+    var v = document.getElementById("BigBlue");
     if (v.style.display === "none") {
        v.style.display = "block";
     } else {
