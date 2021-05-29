8057f67fbb5a034e80922cadf8461730c7abec92
oliver-gorst <oliver.gorst@outlook.com>
2021-05-28 23:03:13 -0400

    initial formatting complete, adding JS for removing images

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function courseDiv() {
+    var v = document.getElementById("course");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ } 
